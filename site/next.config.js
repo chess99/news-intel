15082d90b27df3f5a313b84a1874acd1058e66e2
chess99 <chess99@126.com>
2026-05-11 15:03:10 +0800

    config: remove basePath, update SITE_URL to news.cearl.cc

--- site/next.config.js
+++ site/next.config.js
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/news-intel',
   output: 'export',
   trailingSlash: true,
   images: { unoptimized: true },
   env: {
-    NEXT_PUBLIC_BASE_PATH: '/news-intel',
-    NEXT_PUBLIC_SITE_URL: 'https://cearl.cc',
+    NEXT_PUBLIC_BASE_PATH: '',
+    NEXT_PUBLIC_SITE_URL: 'https://news.cearl.cc',
   },
 }
 
